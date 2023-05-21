c964dddb4153b7a2172301fe681e646eda5c1686
Sam Castleberry <sam@castleberry.xyz>
2023-05-20 23:33:25 -0500

    Fix pinyin support (use local file instead of missing online API)
    pinyin.js copied from https://github.com/glowinthedark/Simplified-and-Traditional-Chinese/blob/master/gb2312-big5/js/pinyin.js

--- res/getPinyin.js
+++ res/getPinyin.js
@@ -4,16 +4,11 @@
 *	Function callback(result) = function that will be called after getPinyin is done
 */
 function getPinyin(character, callback){
-	var requestUrl = "https://cors.io/?https://glosbe.com/transliteration/api?from=Han&dest=Latin&text="
-			+ encodeURI(character)
-			+ "&format=json";
-	$.getJSON(requestUrl, function(data){
-		if(data.result == "ok"){
-			callback(data.text); // return the pinyin of the character
-		} else {
-			console.error("Error occured when fetching Pīnyīn for character " + character + " from glosbe.com. Check if you're connected to the internet and are able to reach https://glosbe.com/a-api and https://cors.io/.");
-			$("#statusTr").addClass("error").removeClass("processing");
-			$("#substatus").html("<b>Error</b> occured when fetching Pīnyīn for character " + character + " from glosbe.com. Check if you're connected to the internet and are able to reach https://glosbe.com/a-api and https://cors.io/. Please read the <a href='#sec:errorHandling'>Error Handling section</a> below.");
+	function getCharPinyin(character){
+		if(Object.hasOwn(pinyin_data, character)) {
+			return pinyin_data[character].split(",")[0];
 		}
-	});
+		return "???"
+	}
+	callback(character.split("").map((elt,idx,arr) => getCharPinyin(elt)).join(" "));
 }
